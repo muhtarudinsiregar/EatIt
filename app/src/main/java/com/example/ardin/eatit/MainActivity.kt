25366587e3cc622e637a3ae85d110f849c6e368c
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-01-24 17:01:00 +0700

    Added styling homescreen + skeleton button

--- app/src/main/java/com/example/ardin/eatit/MainActivity.kt
+++ app/src/main/java/com/example/ardin/eatit/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.ardin.eatit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //sign up button action
+        btnSignUp.setOnClickListener {
+            println("MASUK SINI")
+        }
+
+        //active button action
+        btnSignActive.setOnClickListener {
+
+            println("MASUK SINI")
+        }
     }
 }
